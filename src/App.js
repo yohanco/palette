413fe907f3a53241845f03d2546883b8037c76df
yohanco <52788399+yohanco@users.noreply.github.com>
2019-07-11 14:02:29 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+/*jslint esnext:true, browser:true*/
+/**
+ * @module App
+ */
+export default class App {
+	/**
+	 * Méthode principale. Sera typiquement appelée après le chargement de la page.
+	 */
+	static main() {
+		console.log("Je suis prêt");
+		document.getElementById("app").innerHTML = "La page est chargée";
+	}
+	/**
+	 * Méthode qui permet d'attendre le chargement de la page avant d'éxécuter le script principal
+	 * @returns {Promise} La promesse qui sera résolue après chargement
+	 */
+	static load() {
+		return new Promise(resolve => {
+			window.addEventListener("load", () => {
+				resolve();
+			});
+		});
+	}
+}
